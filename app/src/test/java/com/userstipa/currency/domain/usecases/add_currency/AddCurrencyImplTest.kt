0364151d7c4d849335cfe8295e144bd5c35aca90
Stipa <62395737+UserStipa@users.noreply.github.com>
2023-10-24 21:10:24 +0300

    feature: add currency to my list. Add test for AddCurrencyImpl

---
+++ app/src/test/java/com/userstipa/currency/domain/usecases/add_currency/AddCurrencyImplTest.kt
@@ -0,0 +1,52 @@
+package com.userstipa.currency.domain.usecases.add_currency
+
+import com.userstipa.currency.data.local.PreferencesKeys
+import com.userstipa.currency.domain.model.Currency
+import com.userstipa.currency.testUtil.RepositoryFake
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class AddCurrencyImplTest {
+
+    private lateinit var addCurrency: AddCurrencyImpl
+    private lateinit var repositoryFake: RepositoryFake
+
+    @Before
+    fun setUp() {
+        repositoryFake = RepositoryFake()
+        addCurrency = AddCurrencyImpl(repositoryFake)
+    }
+
+    @Test
+    fun `launch - preferences with values`() = runTest {
+        val currentCurrencies = setOf("bitcoin1", "bitcoin2")
+        repositoryFake.setPreferences(PreferencesKeys.MY_CURRENCIES, currentCurrencies)
+
+        val newCurrency = Currency(
+            id = "bitcoin3",
+            name = "Bitcoin_3",
+            symbol = "BTC"
+        )
+        addCurrency.launch(newCurrency)
+
+        val expectedValue = setOf("bitcoin1", "bitcoin2", "bitcoin3")
+        val actualValue = repositoryFake.getPreferences(PreferencesKeys.MY_CURRENCIES)
+        Assert.assertEquals(expectedValue, actualValue)
+    }
+
+    @Test
+    fun `launch - preferences without values`() = runTest {
+        val newCurrency = Currency(
+            id = "bitcoin3",
+            name = "Bitcoin_3",
+            symbol = "BTC"
+        )
+        addCurrency.launch(newCurrency)
+
+        val expectedValue = setOf("bitcoin3")
+        val actualValue = repositoryFake.getPreferences(PreferencesKeys.MY_CURRENCIES)
+        Assert.assertEquals(expectedValue, actualValue)
+    }
+}
