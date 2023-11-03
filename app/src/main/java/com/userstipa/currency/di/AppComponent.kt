d79b1db0759f0a8fafe73723875f1b476398866b
Stipa <62395737+UserStipa@users.noreply.github.com>
2023-11-03 15:28:14 +0300

    feature: Currency Details. Create DetailsViewModel and GetCurrency usecase

--- app/src/main/java/com/userstipa/currency/di/AppComponent.kt
+++ app/src/main/java/com/userstipa/currency/di/AppComponent.kt
@@ -9,8 +9,10 @@ import com.userstipa.currency.di.domain_modules.MapperModule
 import com.userstipa.currency.di.ui_modules.ViewModelFactoryModule
 import com.userstipa.currency.domain.usecases.add_currency.AddCurrency
 import com.userstipa.currency.domain.usecases.get_all_currencies.GetAllCurrencies
+import com.userstipa.currency.domain.usecases.get_currency.GetCurrency
 import com.userstipa.currency.domain.usecases.remove_currency.RemoveCurrency
 import com.userstipa.currency.domain.usecases.subscribe_my_currencies.SubscribeMyCurrencies
+import com.userstipa.currency.ui.details.DetailsFragment
 import com.userstipa.currency.ui.home.HomeFragment
 import com.userstipa.currency.ui.search_currency.SearchFragment
 import dagger.BindsInstance
@@ -42,10 +44,14 @@ interface AppComponent {
 
     fun useCaseSubscribeMyCurrencies(): SubscribeMyCurrencies
 
+    fun useCaseGetCurrency(): GetCurrency
+
     fun getDispatcherProvider(): DispatcherProvider
 
     fun inject(homeFragment: HomeFragment)
 
     fun inject(searchFragment: SearchFragment)
+
+    fun inject(detailsFragment: DetailsFragment)
 
 }
--- app/src/main/java/com/userstipa/currency/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/userstipa/currency/ui/details/DetailsFragment.kt
@@ -1,18 +1,35 @@
 package com.userstipa.currency.ui.details
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.navArgs
+import com.userstipa.currency.App
 import com.userstipa.currency.databinding.FragmentDetailsBinding
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 class DetailsFragment : Fragment() {
 
+    @Inject
+    lateinit var viewModelFactory: DetailsViewModelFactory
+    private val viewModel by viewModels<DetailsViewModel> { viewModelFactory }
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
     private val args: DetailsFragmentArgs by navArgs()
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        (context.applicationContext as App).appComponent.inject(this)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,8 +37,25 @@ class DetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDetailsBinding.inflate(inflater, container, false)
-        binding.currencyId.text = args.currencyId
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.fetchData(args.currencyId)
+        setObservers()
+    }
+
+    private fun setObservers() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collectLatest { uiState ->
+                    uiState.currency?.let { currency ->
+                        binding.name.text = currency.name
+                    }
+                }
+            }
+        }
     }
 
 
